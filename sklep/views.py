d460656df5d1d9d788cdb4f9c37f97fd8ad400bc
mariasemeniuk80 <maria.semeniuk@infotech.edu.pl>
2026-03-31 19:22:50 +0200

    poprawa html

--- sklep/views.py
+++ sklep/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Product
 
 def home(request):
-    """Pobiera wszystkie produkty z bazy i wysyła je do szablonu home.html."""
+    """Pobiera wszystkie produkty z bazy i wysyła je do szablonu."""
     products = Product.objects.all()
     return render(request, 'sklep/home.html', {'products': products})
