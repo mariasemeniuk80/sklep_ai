55b99e1dc78f55fc8982beec169fd1537acdb634
mariasemeniuk80 <maria.semeniuk@infotech.edu.pl>
2026-03-31 18:59:40 +0200

    baza danych

--- sklep/models.py
+++ sklep/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """reprezentuje kategorię roślin w sklepie."""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    """reprezentuje konkretną roślinę na sprzedaż."""
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
